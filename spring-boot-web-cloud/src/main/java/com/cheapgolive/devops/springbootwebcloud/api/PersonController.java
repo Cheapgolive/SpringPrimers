1d0eb6ccbf4492352a6c3d89bb2862a95eff402a
cheapgolive-Devops <contact@cheapgolive.com>
2017-09-26 23:10:01 -0400

    Added rest controller and model classes

---
+++ spring-boot-web-cloud/src/main/java/com/cheapgolive/devops/springbootwebcloud/api/PersonController.java
@@ -0,0 +1,24 @@
+package com.cheapgolive.devops.springbootwebcloud.api;
+
+import com.cheapgolive.devops.springbootwebcloud.model.Person;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by eml2s on 9/26/2017.
+ */
+@RestController
+@RequestMapping("/api")
+public class PersonController {
+
+    @GetMapping
+    public String hello(){
+
+        return "Hello";
+    }
+
+    @PostMapping
+    public String getName(@RequestBody Person person){
+
+        return person.getName();
+    }
+}
---
+++ spring-boot-web-cloud/src/main/java/com/cheapgolive/devops/springbootwebcloud/model/Person.java
@@ -0,0 +1,18 @@
+package com.cheapgolive.devops.springbootwebcloud.model;
+
+/**
+ * Created by cheapgolive-devops on 9/26/2017.
+ */
+public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
